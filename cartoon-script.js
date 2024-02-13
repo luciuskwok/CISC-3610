c62fa1bf7a22a77d90bdb30d82e223e659a4e418
Lucius Kwok <lk@felttip.com>
2024-02-13 11:57:07 -0500

    Add ground grid, cabin.

--- cartoon-script.js
+++ cartoon-script.js
@@ -42,11 +42,29 @@ skyGradient.addColorStop(1, "#BAAAFF");
 ctx.fillStyle = skyGradient;
 ctx.fillRect(0, 0, 640, 240);
 
+// - Ground
 const groundGradient = ctx.createLinearGradient(0, 240, 0, 480);
-groundGradient.addColorStop(0, "#21781A");
+groundGradient.addColorStop(0, "#04780A");
 groundGradient.addColorStop(1, "#11AA11");
 ctx.fillStyle = groundGradient;
 ctx.fillRect(0, 240, 640, 240);
+
+// = Ground Grid
+ctx.strokeStyle = "#04780A";
+ctx.beginPath();
+for (let x = 0; x < 320 * 8; x += 128) {
+	ctx.moveTo(320 + x, 480);
+	ctx.lineTo(320, 240);
+	ctx.lineTo(320 - x, 480);
+}
+ctx.stroke();
+ctx.beginPath();
+for (let z = 1; z <= 16; z += 0.5) {
+	let y = 240 / z;	
+	ctx.moveTo(0, 240 + y);
+	ctx.lineTo(640, 240 + y);
+}
+ctx.stroke();
 
 // Draw background objects
 // - Sun
@@ -76,6 +94,24 @@ for (let z = 20; z > 0; z--) {
 }
 
 // Draw cabin
+ctx.fillStyle = "#884400";
+ctx.strokeStyle = "#221100";
+ctx.lineJoin = "round";
+ctx.lineWidth = "3.0";
+// - Box
+drawCenteredRect(ctx, 320, 240, 144, 96);
+// - Roof
+ctx.beginPath();
+addCenteredTriangle(ctx, 320, 172, 172, 40);
+ctx.fill();
+ctx.stroke();
+// - Door
+ctx.fillStyle = "#442200";
+drawCenteredRect(ctx, 320, 256, 32, 64);
+// - Windows
+drawCenteredRect(ctx, 320 - 44, 240, 24, 24);
+drawCenteredRect(ctx, 320 + 44, 240, 24, 24);
+
 
 // Text
 ctx.textAlign = "center";
