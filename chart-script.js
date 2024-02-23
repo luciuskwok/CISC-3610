67a97e1d23f53ff74bd1c7743d69dbab5c339bb9
Lucius Kwok <lk@felttip.com>
2024-02-22 21:47:58 -0500

    Add files for Chart (JSON Fruit) Lab

---
+++ chart-script.js
@@ -0,0 +1,12 @@
+// chart-script.js
+
+// Functions
+
+
+// == Main Script ==
+
+// Get canvas and drawing context
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+
