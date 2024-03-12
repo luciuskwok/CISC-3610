b70fbb1decfc42b989ad087f6d988e59f91bd6ba
Lucius Kwok <lk@felttip.com>
2024-03-11 20:31:32 -0400

    Simplified audio code

--- audio-script.js
+++ audio-script.js
@@ -5,27 +5,27 @@
 
 // Play sound
 function playSound(snd) {
-	console.log("Play sound "+snd);
+	//console.log("Play sound "+snd);
 	if (snd != null) {
-		let sndObj = document.getElementById(snd);
+		let sndObj = new Audio("snd/"+snd+".wav");
 		sndObj.play(document);
 	}
 }
 
 // Set up button handlers
-document.getElementById('play1').addEventListener('click', ({target}) => { playSound("snd-boing"); });
-document.getElementById('play2').addEventListener('click', ({target}) => { playSound("snd-clink-klank"); });
-document.getElementById('play3').addEventListener('click', ({target}) => { playSound("snd-eep"); });
+document.getElementById('play1').addEventListener('click', ({target}) => { playSound("boing"); });
+document.getElementById('play2').addEventListener('click', ({target}) => { playSound("clink-klank"); });
+document.getElementById('play3').addEventListener('click', ({target}) => { playSound("eep"); });
 
-document.getElementById('play4').addEventListener('click', ({target}) => { playSound("snd-quack"); });
-document.getElementById('play5').addEventListener('click', ({target}) => { playSound("snd-sosumi"); });
-document.getElementById('play6').addEventListener('click', ({target}) => { playSound("snd-uh-oh"); });
+document.getElementById('play4').addEventListener('click', ({target}) => { playSound("quack"); });
+document.getElementById('play5').addEventListener('click', ({target}) => { playSound("sosumi"); });
+document.getElementById('play6').addEventListener('click', ({target}) => { playSound("uh-oh"); });
 
-document.getElementById('play7').addEventListener('click', ({target}) => { playSound("snd-chu-toy"); });
-document.getElementById('play8').addEventListener('click', ({target}) => { playSound("snd-indigo"); });
-document.getElementById('play9').addEventListener('click', ({target}) => { playSound("snd-monkey"); });
+document.getElementById('play7').addEventListener('click', ({target}) => { playSound("chu-toy"); });
+document.getElementById('play8').addEventListener('click', ({target}) => { playSound("indigo"); });
+document.getElementById('play9').addEventListener('click', ({target}) => { playSound("monkey"); });
 
-document.getElementById('play10').addEventListener('click', ({target}) => { playSound("snd-moof"); });
-document.getElementById('play11').addEventListener('click', ({target}) => { playSound("snd-whit"); });
+document.getElementById('play10').addEventListener('click', ({target}) => { playSound("moof"); });
+document.getElementById('play11').addEventListener('click', ({target}) => { playSound("whit"); });
 
 
